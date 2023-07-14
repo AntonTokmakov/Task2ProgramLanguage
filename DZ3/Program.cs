8c5f1859071e005847a93fd3ae8b1f100a4777a1
Anton <antontokmakov757@gmail.com>
2023-07-14 15:34:39 +0700

    Third task complited

---
+++ DZ3/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Программа которая выдает таблицу кубов чисел");
+int count = int.Parse(Console.ReadLine());
+Console.WriteLine("Ответ");
+for (int i = 1; i < count+1; i++)
+{
+    Console.Write(Math.Pow(i, 3) + " ");
+}
